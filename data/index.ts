3273282816323557105770a6d30480978ec65eb6
RichardMiruka <richardonsare15@gmail.com>
2024-09-20 14:32:17 +0300

    dependancy installations

--- data/index.ts
+++ data/index.ts
@@ -34,7 +34,7 @@ export const gridItems = [
         imgClassName: "",
         titleClassName: "justify-center",
         img: "",
-        spareImg: "",
+        spareImg: "", 
     },
     {
         id: 4,
