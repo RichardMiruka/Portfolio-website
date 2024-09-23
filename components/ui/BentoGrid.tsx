903b305aaedcab6dc575133412a3a7942562150f
RichardMiruka <richardonsare15@gmail.com>
2024-09-23 14:50:56 +0300

    email address reference

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -67,7 +67,7 @@ export const BentoGridItem = ({
     };
 
     const handleCopy = () => {
-        const text = "hsu@jsmastery.pro";
+        const text = "richardmiruka96@gmail";
         navigator.clipboard.writeText(text);
         setCopied(true);
     };
